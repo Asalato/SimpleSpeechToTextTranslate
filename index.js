72d92b8d88c389b0805ad66d38be50bd345850fd
Masaki Mita <mimimita3131@gmail.com>
2021-04-01 22:14:23 +0900

    v1.2.1
    bug fix

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function editStyleByClass(propertyName, value){
     for (let i = 0; i < all.length; i++) {
         all[i].style[propertyName] = value;
     }
-    document.cookie = propertyName + ':' + value;
+    document.cookie = propertyName + '=' + value;
 }
 
 window.addEventListener('load', () => {
